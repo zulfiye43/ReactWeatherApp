aa39e1bf0cfe175944d6711526393a5dcf099c83
Zülfiye Cakmak <49038239+zulfiye43@users.noreply.github.com>
2023-12-10 22:05:54 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+const [data, setData] = useState({})
+const [location, setLocation] = useState('')
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=0115abc59d7d17b7ab81aeb6e0eaf939`;
+
+const searchLocation = (event) => {
+  if(event.key === 'Enter'){
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+}
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text">
+        </input>
+      </div>
+      <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>{data.name}</p>
+        </div>
+        <div className="temp">
+  {data.main ? <h1>{Math.ceil(data.main.temp)}°C</h1> : null}
+</div>
+
+        <div className="description">
+          {data.weather ? <p>{data.weather[0].main}</p>: null}
+        </div>
+      </div>
+      
+      {data.name != undefined &&
+      <div className="bottom">
+        <div className="feels">
+  {data.main ? <p className="bold">{Math.ceil(data.main.feels_like)}°C</p> : null}
+  <p>Feels Like</p>
+</div>
+<div className="humidity">
+  {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+  <p>Humidity</p>
+</div>
+<div className="wind">
+{data.wind ? <p className="bold">{Math.ceil(data.wind.speed)} km/h</p> : null}
+  <p>Wind Speed</p>
+</div>
+      </div>
+       }
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
